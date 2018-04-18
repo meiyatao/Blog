797857ee5361cb8572a1ba9f8b1ef971e951697b
meiyatao <18345969260@163.com>
2018-04-18 17:32:37 +0800

    测试提交222

--- src/main/java/com/Blog/controller/test.java
+++ src/main/java/com/Blog/controller/test.java
@@ -5,9 +5,6 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("abc");
-		
-		
-		
 		System.out.print("abc");
 		System.out.print("abc");
 		
